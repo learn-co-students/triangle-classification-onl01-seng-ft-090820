1c6aa6fb36b3b10280fc47eabc18de5f117c50bb
doosemavis <moosedavis2011@gmail.com>
2020-09-25 08:51:56 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,11 +7,23 @@ class Triangle
     @side_3 = side_3
   end 
 
+  def kind
+    if (side_1 < 0 || side_2 < 0 || side_3 < 0) || (side_1 + side_2 <= side_3 || side_2 + side_3 <= side_1 || side_1 + side_3 <= side_2)
+      raise TriangleError
+    elsif side_1 == 0 && side_2 == 0 && side_3 == 0
+      raise TriangleError
+    elsif side_1 == side_2 && side_2 == side_3
+        :equilateral
+    elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
+        :isosceles
+    else 
+        :scalene
+    end
+  end 
+ 
 
   class TriangleError < StandardError
-    def error_message
-      puts "Error: incorrect triangle defined, please try again."
-    end 
-  end   
-end
+  end  
+  
+end 
 
