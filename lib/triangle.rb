fefe726391feac7e8ff5ce4e370c95139d29276c
Daniyella Harmon <daniyellaharmon@gmail.com>
2020-09-29 09:23:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,5 +7,20 @@ class Triangle
     @sides.sort!
   end 
   
+  #instance method
+  def kind
+    if @sides.any?{|side| side <= 0} || ((@sides[0] + @sides[1]) <= @sides[2])
+      raise TriangleError
+    elsif @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
   
+  class TriangleError < StandardError
+    
+  end
 end
